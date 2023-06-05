87b3f3add5a701430ebc9a43c3951acba4a7e3c3
pt74847781 <ariel_sc_2001@hotmail.com>
2023-06-05 11:32:29 -0500

    first commit

---
+++ src/test/java/pe/edu/idat/DSW1EC1L11/Dsw1Ec1L11ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.DSW1EC1L11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1Ec1L11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
